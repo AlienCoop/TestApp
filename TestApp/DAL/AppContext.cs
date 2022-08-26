c22404f4bc37debae5d94ded0892d02e4aa21e7a
AlienCoop <78314439+AlienCoop@users.noreply.github.com>
2022-08-26 18:21:24 +0500

    Добавьте файлы проекта.

---
+++ TestApp/DAL/AppContext.cs
@@ -0,0 +1,23 @@
+﻿using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Data.Entity;
+using TestApp.Models;
+using Task = TestApp.Models.Task;
+
+namespace TestApp.DAL
+{
+    public class AppContext : DbContext
+    {
+
+        public AppContext() : base("AppContext")
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Task> Tasks { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
